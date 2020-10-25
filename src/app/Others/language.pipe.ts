eec05a44590b4d9396d5658466e8ecea5c43a31f
RamyaAmarnath <ramyaramesh465@gmail.com>
2020-10-25 09:56:01 +0530

    2 files modified

--- src/app/Others/language.pipe.ts
+++ src/app/Others/language.pipe.ts
@@ -1,12 +1,26 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'language'
+  name: "language"
 })
 export class LanguagePipe implements PipeTransform {
-
-  transform(value: any, args?: any): any {
-    return 'Maths';
+  languageset = {
+    ENGLISH: {
+      HI: "Hi"
+    },
+    TAMIL: {
+      HI: "இருக்கிறது"
+    }
+  };
+  transform(value: string, args?: any): any {
+    console.log(value);
+    console.log(args);
+    value = value.toUpperCase();
+    // return "Maths";
+    let language = "TAMIL";
+    if (this.languageset[language][value]) {
+      return this.languageset[language][value];
+    }
+    return "Maths";
   }
-
 }
