93d882c6376c3bde4cac65e85c648556e3e2682f
RamyaAmarnath <ramyaramesh465@gmail.com>
2020-10-25 09:33:27 +0530

    1 file modified

--- src/app/Others/language.pipe.ts
+++ src/app/Others/language.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class LanguagePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return nul;
+    return 'Maths';
   }
 
 }
