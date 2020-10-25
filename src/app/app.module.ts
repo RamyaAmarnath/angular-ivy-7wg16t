e7ccf7893f6acabb0f66073717d736c9c4bb4dab
RamyaAmarnath <ramyaramesh465@gmail.com>
2020-10-25 10:22:31 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { LanguagePipe } from './Others/language.pipe';
 import { LoginComponent } from './Components/login/login.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, LanguagePipe, LoginComponent ],
+  declarations: [ AppComponent, HelloComponent, LanguagePipe, LoginComponent, NavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
