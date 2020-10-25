fe645430ffc5e6adc571a0fd262b86c6cc1a9f83
RamyaAmarnath <ramyaramesh465@gmail.com>
2020-10-25 09:12:52 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  now=new Date();
+  name = 'Angular ' + VERSION.major;
+}
