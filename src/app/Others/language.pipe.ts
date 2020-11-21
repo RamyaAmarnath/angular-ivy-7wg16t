04ac7672aea06259bc24b8484d540b005f02595f
RamyaAmarnath <ramyaramesh465@gmail.com>
2020-11-21 17:08:15 +0530

    2 files modified

--- src/app/Others/language.pipe.ts
+++ src/app/Others/language.pipe.ts
@@ -9,25 +9,37 @@ export class LanguagePipe implements PipeTransform {
       HI: "Hi",
       HELLO: "Hello",
       LOGIN: "Login",
-      SIGNUP: "Signup"
+      SIGNUP: "Signup",
+      USERNAME: "Username",
+      PASSWORD: "Password",
+      RETYPE PASSWORD: "Retype Password"
     },
     TAMIL: {
       HI: "இருக்கிறது",
       HELLO: "வணக்கம்",
       LOGIN: "உள்நுழைய",
-      SIGNUP: "பதிவுபெறுதல்"
+      SIGNUP: "பதிவுபெறுதல்",
+      USERNAME: "பயனர்பெயர்",
+      PASSWORD: "கடவுச்சொல்",
+      RETYPE PASSWORD: "கடவுச்சொல்லை மீண்டும் தட்டச்சு செய்க"
     },
     HINDI: {
       HI: "இருக்கிறது",
       HELLO: "नमस्ते",
       LOGIN: "H Login",
-      SIGNUP: "साइन अप करें"
+      SIGNUP: "साइन अप करें",
+      USERNAME: "उपयोगकर्ता नाम",
+      PASSWORD: "कुंजिका",
+      RETYPE PASSWORD: "पासवर्ड फिर से लिखें"
     },
     FRENCH: {
       HI: "EST",
       HELLO: "BONJOUR",
       LOGIN: "F Login",
-      SIGNUP: "s'inscrire"
+      SIGNUP: "s'inscrire",
+      USERNAME: "Nom d'utilisateur",
+      PASSWORD: "Mot de passe",
+      RETYPE PASSWORD: "Retaper le mot de passe"
     }
   };
   transform(value: string, args?: string): any {
