7830bbf74e621a6c6dd615454d867a7ecf10a8a9
RamyaAmarnath <ramyaramesh465@gmail.com>
2020-12-13 19:47:20 +0530

    2 files modified

--- src/app/Components/dummy/dummy.component.ts
+++ src/app/Components/dummy/dummy.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnDestroy, OnInit, VERSION } from "@angular/core";
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  VERSION
+} from "@angular/core";
 
 @Component({
   selector: "app-dummy",
   templateUrl: "./dummy.component.html",
   styleUrls: ["./dummy.component.css"]
 })
-export class DummyComponent implements OnInit, OnDestroy {
+export class DummyComponent
+  implements
+    OnInit,
+    OnDestroy,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked {
   hello = "hi";
   now = new Date();
   name = "Angular " + VERSION.major;
   showme = true;
   cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+  count=0;
   constructor() {
     console.log("constructor");
   }
@@ -18,10 +35,26 @@ export class DummyComponent implements OnInit, OnDestroy {
   ngOnInit() {
     console.log("ngOnInit");
   }
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit");
+  }
+  ngAfterContentChecked() {
+    console.log("ngAfterContentChecked");
+  }
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit");
+  }
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked");
+  }
   chsngeme() {
     // alert('hi');
     this.hello = "boy";
     this.showme = !this.showme;
+  }
+  keytriggered(){
+    this.count++;
+    console.log(this.count);
   }
   ngOnDestroy() {
     console.log("ngOnDestroy");
