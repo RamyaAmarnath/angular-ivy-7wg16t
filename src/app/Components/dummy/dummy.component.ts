f5f2b1819d7ee05259774137022b4aad63fcea89
RamyaAmarnath <ramyaramesh465@gmail.com>
2020-12-13 19:32:35 +0530

    1 file modified

--- src/app/Components/dummy/dummy.component.ts
+++ src/app/Components/dummy/dummy.component.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit, VERSION } from "@angular/core";
+import { Component, OnDestroy, OnInit, VERSION } from "@angular/core";
 
 @Component({
   selector: "app-dummy",
   templateUrl: "./dummy.component.html",
   styleUrls: ["./dummy.component.css"]
 })
-export class DummyComponent implements OnInit {
+export class DummyComponent implements OnInit, OnDestroy {
   hello = "hi";
   now = new Date();
   name = "Angular " + VERSION.major;
   showme = true;
   cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
-  constructor() {}
+  constructor() {
+    console.log("constructor");
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log("ngOnInit");
+  }
   chsngeme() {
     // alert('hi');
     this.hello = "boy";
     this.showme = !this.showme;
+  }
+  ngOnDestroy() {
+    console.log("ngOnDestroy");
+    alert("Your changes will be lost");
   }
 }
