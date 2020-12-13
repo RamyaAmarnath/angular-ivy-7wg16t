2ec044d6fe5dc2fc60dda21ea2cfa8d9e5562394
RamyaAmarnath <ramyaramesh465@gmail.com>
2020-12-13 18:58:47 +0530

    5 files modified, 1 added

--- src/app/Components/employee-detail/employee-detail.component.ts
+++ src/app/Components/employee-detail/employee-detail.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-employee-detail',
-  templateUrl: './employee-detail.component.html',
-  styleUrls: ['./employee-detail.component.css']
+  selector: "app-employee-detail",
+  templateUrl: "./employee-detail.component.html",
+  styleUrls: ["./employee-detail.component.css"]
 })
 export class EmployeeDetailComponent implements OnInit {
+  employee = {
+    Name: "Aurora",
+    Age: 53,
+    Email: "ante.blandit@disparturient.ca",
+    Street: "493 Iaculis Rd.",
+    City: "Lewiston",
+    Zip: "42591-180",
+    Region: "Maine",
+    Country: "Sudan",
+    Info: "est. Nunc ullamcorper, velit in aliquet lobortis, nisi nibh lacinia",
+    EmployeeID: 101,
+    DateOfJoining: "10/06/2019",
+    PhoneNumber: "1628101902299"
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { SignupComponent } from "./Components/signup/signup.component";
 import { DummyComponent } from "./Components/dummy/dummy.component";
 import { ErrorComponent } from "./Components/error/error.component";
 import { PaginationComponent } from "./Components/pagination/pagination.component";
+import { EmployeeDetailComponent } from "./Components/employee-detail/employee-detail.component";
+import { EmployeeListComponent } from "./Components/employee-list/employee-list.component";
 const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "signup", component: SignupComponent },
   { path: "dummy", component: DummyComponent },
   { path: "error", component: ErrorComponent },
-  { path: "pagination", component: PaginationComponent }
+  { path: "pagination", component: PaginationComponent },
+  { path: "employee-list", component: EmployeeListComponent },
+  { path: "employee-detail", component: EmployeeDetailComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
