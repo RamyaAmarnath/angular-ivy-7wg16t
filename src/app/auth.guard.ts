965afd097a8342b357004c146dd626f649823bc1
RamyaAmarnath <ramyaramesh465@gmail.com>
2020-12-20 19:32:03 +0530

    1 file modified

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -8,7 +8,7 @@ import { Observable } from "rxjs";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  validuser = false;
+  validuser = true;
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
